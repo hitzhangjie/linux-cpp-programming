afbd469edc46f7783027e471e5559e6133d6ee1f
hitzhangjie <hit.zhangjie@gmail.com>
2022-06-28 09:13:22 +0000

    format the c code

--- alignment/main.c
+++ alignment/main.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 
+typedef struct
+{
+    char sex;
+    int age;
+} Person;
+
 // because it's packed, so sizeof is 5 bytes
 // 1 + 4 = 5 bytes
-typedef struct __attribute__ ((packed))
+typedef struct __attribute__((packed))
 {
     char sex;
     int age;
 } Student;
 
 // this way: 1 + 4 + 3padding + 4 = 12 bytes
-struct StudentX {
-    char sex __attribute__ ((aligned (1)));
-    int age __attribute__ ((packed));
-    int xxx __attribute__ ((aligned(4)));
+struct StudentX
+{
+    char sex __attribute__((aligned(1)));
+    int age __attribute__((packed));
+    int xxx __attribute__((aligned(4)));
 };
 
 // this way, the sizeof StudentY will be 16 bytes
 // 8 + 8 = 16 bytes
-struct StudentY {
-    char sex __attribute__ ((aligned (8)));
-    int age __attribute__ ((aligned (8)));
+struct StudentY
+{
+    char sex __attribute__((aligned(8)));
+    int age __attribute__((aligned(8)));
 };
 
 // this way, add attributes to the struct means this struct:
@@ -32,21 +40,26 @@ struct StudentY {
 // telling the compiler to try to align the struct members in this way:
 // - if aligned (n) is too small, use default value, like char:1 int:4
 // - if aligned (n) is bigger than default values, try to align to bigger boundary.
-typedef struct __attribute__ ((aligned (4))) 
+typedef struct __attribute__((aligned(4)))
 {
-    char sex ;
-    int age ;
+    char sex;
+    int age;
 } StudentZ;
 
 int main(int argc, char **argv)
 {
     int n = 100;
     printf("n: %d\n", n);
-    printf("sizeof(int): %lu\n", sizeof(n));
+    printf("sizeof(int): %lu, address: %p\n", sizeof(n), &n);
+
+    Person p;
+    printf("sizeof(person): %lu\n", sizeof(p));
+    printf("person address: %p\n", &p);
+    printf("person.sex address: %p\n", &p.sex);
+    printf("person.age address: %p\n", &p.age);
 
     Student s;
     printf("sizeof(student): %lu\n", sizeof(s));
-
     struct StudentX x;
     printf("sizeof(studentx): %lu\n", sizeof(x));
 
